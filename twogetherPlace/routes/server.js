2ef6fb026fccc764d2a554f082901c8a5e51e039
heerim <90364507+heerim09@users.noreply.github.com>
2023-07-24 20:42:47 +0900

    잘들가세요

--- twogetherPlace/routes/server.js
+++ twogetherPlace/routes/server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-res.sendFile(__dirname + '/placeInfo.html');
+res.sendFile(__dirname + '../placeInfo.html');
 })
 
 app.listen(3000);
