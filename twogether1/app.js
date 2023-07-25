95e02a2a0a2958dd4223a2e56d69438dce82c6ac
wngml02 <wngml021211@gmail.com>
2023-07-25 23:50:40 +0900

    mani nodejs express

--- twogether1/app.js
+++ twogether1/app.js
@@ -1,3 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('', function(req, res) {
+    return res.sendFile(__dirname + '/index.html');
+});
+app.get('/page', function(req, res) {
+    return res.send('/page');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+/*
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -24,18 +39,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-module.exports = app;
+module.exports = app;*/
