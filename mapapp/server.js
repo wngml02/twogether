ae815aa0c48ccba82e02402a4112ea8a39ff5dfb
wngml02 <wngml021211@gmail.com>
2023-07-27 22:02:56 +0900

    결과가 164개밖에 안나옴,,,

--- mapapp/server.js
+++ mapapp/server.js
@@ -2,6 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let request = require('request');
+let options = {
+    'method': 'GET',
+    'url': 'https://apis.data.go.kr/B551011/GreenTourService1/areaBasedList1?numOfRows=5000&MobileOS=ETC&MobileApp=twogether&_type=json&serviceKey=eJJp4cIsk7s0g0Dk2t1GVKzlyGledlhGUr8gHQiNewRzOBAPt0v%2FFOnEWGcmM5R6X6k5qn7FjIuXgqqB9o4w%2BQ%3D%3D',
+    'headers': {
+        'Cookie': 'WMONID=sN2sN5XVW3y'
+    }
+};
+const count = 5000;
+request(options, function(error, response, body) {
+    if (error) {
+        throw new Error(error);
+    }
+    let info = JSON.parse(body);
+
+    for (i in info['response']['body']['items']['item']) {
+        console.log('이름 : ' + i + info['response']['body']['items']['item'][i]['title']);
+        console.log('주소 : ' + info['response']['body']['items']['item'][i]['addr']);
+        console.log(" ")
+    }
+    i++;
+});
 app.use(express.static(__dirname + "/public"));
 
 app.get('', function(req, res) {
