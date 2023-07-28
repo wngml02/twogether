7b7fb1c08c027151c2536c6c3a56913f52d14fde
wngml02 <wngml021211@gmail.com>
2023-07-28 21:44:41 +0900

    흠

--- seeingApp/app.js
+++ seeingApp/app.js
@@ -6,38 +6,37 @@ const port = 3000;
 
 let request = require('request');
 let options = {
-  'method': 'GET',
-  'url': 'https://apis.data.go.kr/B551011/GreenTourService1/areaBasedList1?numOfRows=5&pageNo=1&MobileOS=ETC&MobileApp=App&_type=json&serviceKey=iPOcFKrhHgswObtTYryGrWDTZq4ck8a%2FGIYMAjRBDVO3DnY2O70fCDzT4Dzj2IWMSdJCb7%2F%2BMsO52yqttO72Zw%3D%3D',
-  'headers': {
-    'Cookie': 'NCPVPCLB=53dc2963a8054bd57870a8b2355dc148919c5a02851f15d4ffafa945a766b4a1'
-  }
+    'method': 'GET',
+    'url': 'https://apis.data.go.kr/B551011/GreenTourService1/areaBasedList1?numOfRows=5&pageNo=1&MobileOS=ETC&MobileApp=App&_type=json&serviceKey=iPOcFKrhHgswObtTYryGrWDTZq4ck8a%2FGIYMAjRBDVO3DnY2O70fCDzT4Dzj2IWMSdJCb7%2F%2BMsO52yqttO72Zw%3D%3D',
+    'headers': {
+        'Cookie': 'NCPVPCLB=53dc2963a8054bd57870a8b2355dc148919c5a02851f15d4ffafa945a766b4a1'
+    }
 };
+module.exports = app;
+request(options, function(error, response, body) {
+    if (error) {
+        throw new Error(error);
+    }
+    let info = JSON.parse(body);
 
-request(options, function (error, response, body) {
-  if (error) {
-    throw new Error(error);
-  }
-  let info = JSON.parse(body);
-
-  for (i in info['reponse']['body']['items']['item']) {
-    console.log('주소:'+info['response']['body']['items']['item'][i]['addr']);
-    console.log('설명:'+info['response']['body']['items']['item'][i]['summary']);
-    console.log('전화번호:'+info['response']['body']['items']['item'][i]['tel']);
-    console.log('');
-  }
+    for (i in info['reponse']['body']['items']['item']) {
+        console.log('설명:' + info['response']['body']['items']['item'][i]['summary']);
+        console.log('전화번호:' + info['response']['body']['items']['item'][i]['tel']);
+        console.log('');
+    }
 });
 
-module.exports = app;
+
 
 app.get('', function(req, res) {
-  return res.sendFile(__dirname + '/sightSeeing.html');
+    return res.sendFile(__dirname + '/sightSeeing.html');
 });
 app.get('/page', function(req, res) {
-  return res.send('/page');
+    return res.send('/page');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 });
 
 
