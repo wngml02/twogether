6df05610285cee2cf016904b085b893e37a245ee
wngml02 <wngml021211@gmail.com>
2023-07-28 23:29:29 +0900

    map 연결

--- mainApp/app.js
+++ mainApp/app.js
@@ -9,6 +9,9 @@ app.get('', function(req, res) {
 app.get('/placeInfo', function(req, res) {
     return res.sendFile(__dirname + '/placeInfo.html');
 });
+app.get('/map', function(req, res) {
+    return res.sendFile(__dirname + '/map.html');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
