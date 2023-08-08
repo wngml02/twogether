e82ddc77a610d7b46ea4b323dd6c2e9f7f70fb4b
heerim <90364507+heerim09@users.noreply.github.com>
2023-08-08 16:53:26 +0900

    tree

--- namuGrow/namuGrow.js
+++ namuGrow/namuGrow.js
@@ -1,4 +1,11 @@
-let icon = document.querySelector('ion-icon');
+let icon = document.querySelectorAll('ion-icon');
+/*
 icon.onclick = function(){
   icon.classList.toggle('active');
 }
+*/
+icon.forEach(function(icons){
+  icons.onclick = function(){
+    icons.classList.toggle('active');
+  }
+})
