0e1f114d646a706e0283cae6508cb6e6daa760a8
Chaeyeon <codus1114@gmail.com>
2023-08-24 01:39:51 +0900

    엉엉

--- mainApp/config/db.js
+++ mainApp/config/db.js
@@ -1,11 +1,38 @@
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+var options = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'TWOGETHER'
+};
+var sessionStore = new MySQLStore(options);
+
+app.use(session({
+    secret:"sleepy",
+    resave:false,
+    saveUninitialized:true,
+    store:sessionStore
+}))
+
 const mysql = require('mysql');
-const dbInfo = mysql.createConnection({
+const dbInfo = {
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'minseok0326',
+    password: '',
     database: 'TWOGETHER'
-});
+};
 
-dbInfo.connect();
-module.exports = dbInfo;
+module.exports = {
+    init: function() {
+        return mysql.createConnection(dbInfo);
+    },
+    connect: function(conn) {
+        conn.connect(function(err) {
+            if (err) console.error('mysql 연결 에러 : ' + err);
+            else console.log('mysql 연결 성공');
+        });
+    }
+};
