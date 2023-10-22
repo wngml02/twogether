55646bebfd8a417a2254671ab3936e2e4d8a02e0
heerim <90364507+heerim09@users.noreply.github.com>
2023-10-22 19:14:36 +0900

    일부검색&표 정리

--- mainApp/public/javascripts/trsmic.js
+++ mainApp/public/javascripts/trsmic.js
@@ -20,7 +20,7 @@ const getXMLfromAPI = async() => {
 };
 
 // 검색 기능
-
+/*
 function searchTourismInfo() {
     var searchInput = document.getElementById('sh').value;
     console.log(searchInput);
@@ -75,3 +75,80 @@ function searchTourismInfo() {
             console.error('API 요청 중 오류 발생:', error);
         });
 }
+*/
+// 모든 관광안내소 정보를 받아오는 함수
+const getAllTourismInfo = async () => {
+  try {
+    const reqURL = `${url}${queryParams}`;
+    const response = await fetch(reqURL);
+    const jsonData = await response.text();
+
+    // JSON 데이터를 파싱
+    const allTourismInfo = JSON.parse(jsonData).response.body.items;
+    console.log(allTourismInfo);
+
+    return allTourismInfo;
+  } catch (error) {
+    console.error('오류 발생:', error);
+    throw error;
+  }
+};
+
+// 검색 기능
+async function searchTourismInfo() {
+  var searchInput = document.getElementById('sh').value;
+  console.log(searchInput);
+
+  try {
+    // 모든 관광안내소 정보 받아오기
+    const allTourismInfo = await getAllTourismInfo();
+
+    // 검색어가 없으면 모든 정보 출력
+    if (!searchInput) {
+      displayTourismInfo(allTourismInfo);
+      return;
+    }
+
+    // 검색어가 있는 경우, 검색어가 포함된 정보만 필터링하여 출력
+    const filteredTourismInfo = allTourismInfo.filter(item =>
+      item.trsmicNm.includes(searchInput)
+    );
+
+    displayTourismInfo(filteredTourismInfo);
+  } catch (error) {
+    console.error('API 요청 중 오류 발생:', error);
+  }
+}
+//엔터키
+document.getElementById('sh').addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
+    searchTourismInfo();
+  }
+});
+
+// 관광안내소 정보를 화면에 출력하는 함수
+function displayTourismInfo(infoArray) {
+  const tableBody = document.getElementById('tableBody');
+  tableBody.innerHTML = '';
+
+  if (infoArray.length === 0) {
+    tableBody.innerHTML = '<tr><td colspan="3">검색 결과가 없습니다.</td></tr>';
+    return;
+  }
+
+  infoArray.forEach(item => {
+    const row = document.createElement('tr');
+    const openTime = item.summerOperOpenHhmm;
+    const closeTime = item.summerOperCloseHhmm;
+    const combinedTime = `${openTime}-${closeTime}`;
+
+    row.innerHTML = `
+      <td style="color: #142c07;">${item.trsmicNm}</td>
+      <td style="color: #142c07;">${item.rdnmadr}</td>
+      <td style="color: #142c07;"><a href="${item.homepageUrl}" style="color: #142c07;">${item.homepageUrl}</a></td>
+      <td style="color: #142c07;">${combinedTime}</td>
+    `;
+    tableBody.appendChild(row);
+  });
+}
+
