9b456eaf3763cc2daeb61d0dbeaf5abedd40514d
heerim <90364507+heerim09@users.noreply.github.com>
2023-10-23 07:07:41 +0900

    encode임

--- mainApp/public/javascripts/trsmic.js
+++ mainApp/public/javascripts/trsmic.js
@@ -1,6 +1,6 @@
 // 실제 API 엔드포인트로 대체해야 합니다.
 var url = 'http://api.data.go.kr/openapi/tn_pubr_public_trsmic_api'; /*URL*/
-var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + `dTbTVKBhYqclSFuHS2cAH+X9k1Y8T35mSxoJMrsH19fRZad/F4rDnJEONc4r366Y+08r8SlY5AXUi8rOLGH8fw==`; /*Service Key*/
+var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + `dTbTVKBhYqclSFuHS2cAH%2BX9k1Y8T35mSxoJMrsH19fRZad%2FF4rDnJEONc4r366Y%2B08r8SlY5AXUi8rOLGH8fw%3D%3D`; /*Service Key*/
 queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
 queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* */
 queryParams += '&' + encodeURIComponent('type') + '=' + encodeURIComponent('json'); /* */
